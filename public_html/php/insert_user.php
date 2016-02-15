050b58f0b2cb0228a2ada78ece610f1045716368
Montana <montanawong@gmail.com>
2016-02-15 18:30:53 -0500

    added User class

--- public_html/php/insert_user.php
+++ public_html/php/insert_user.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once '../../resources/config.php';
+require_once('Models/User.php');
 include 'Functions.php';
 
 
@@ -8,12 +9,6 @@ if(!$_POST){
     Functions::redirect('registration.php?status=error');
 }
 
-// Create connection
-$conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
 /*
  * This is how to visualize the $_POST array that is sent from the html form on register.php
  * $_POST = ['first_name'=> 'John', 'last_name'=> 'Doe', 'password'=>'password', 'email'=>'joe@example.com' ....]
@@ -23,29 +18,28 @@ if ($conn->connect_error) {
  *
  */
 
-$first_name = $_POST['first_name'];
-$last_name = $_POST['last_name'];
+$firstName = $_POST['firstName'];
+$lastName = $_POST['lastName'];
 $email = $_POST['email'];
-$student_number = $_POST['student_number'];
+$studentNumber = $_POST['studentNumber'];
 //define password hash function later
-$password_hash = $_POST['password'];
+$passwordHash = $_POST['password'];
 $address = $_POST['address'];
 $major = $_POST['major'];
 
-$sql = "INSERT INTO user (first_name, last_name, password_hash, email, student_number, address, major)
-VALUES ('{$first_name}', '{$last_name}', '{$password_hash}', '{$email}', '{$student_number}', '$address', '{$major}');";
 
+$user = new User(
+    $firstName, $lastName, $email, $studentNumber, $passwordHash, $address, $major
+);
 
-//$sql = "INSERT INTO user (first_name, last_name, password_hash, email, student_number, address, major)
-//VALUES ('$_POST[first_name]', '$_POST[last_name]', '$_POST[password]', '$_POST[email]', '$_POST[student_number]',  '$_POST[address]', '$_POST[major]')";
-
-if ($conn->query($sql) === TRUE) {
+if($user->register() == true){
     echo "New record created successfully";
-    Functions::redirect('../index.php?status=registersuccess');
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
+    Functions::redirect('../index.php?status=register_success');
+}
+else{
+    echo "Error: ";
+    Functions::redirect('../register.php?status=error2');
 }
 
-$conn->close();
 
 ?>
