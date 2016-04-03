6c162693d89df4290951fda0c10c208ebc76887f
Montana <montanawong@gmail.com>
2016-04-03 15:04:37 -0400

    transferred admin to php

---
+++ src/src/edu/uga/cs/recdawgs/entity/Administrator.php
@@ -0,0 +1,11 @@
+<?php
+
+/** This class represents an Administrator user in the RecDawgs system.  It has no additional attributes beyond those inherited from User.
+*
+*/
+public interface Administrator extends User
+{
+
+}
+
+?>
