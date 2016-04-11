ff533e0962307f16e9816b52254c38830cddbabd
Montana <montanawong@gmail.com>
2016-04-11 09:07:38 -0400

    adding obj layer tests

---
+++ src/src/edu/uga/cs/recdawgs/tests/ObjectLayerImplTest.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: montanawong
+ * Date: 4/11/16
+ * Time: 09:04
+ */
+
+namespace edu\uga\cs\recdawgs\tests;
+use edu\uga\cs\recdawgs\object\impl;
+
+class ObjectLayerImplTest extends \PHPUnit_Framework_TestCase{
+
+}
