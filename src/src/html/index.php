bc13e1b955478f50c1655253d873b248de7b1cb0
taihoang <taihoang@uga.edu>
2016-04-23 18:27:51 -0400

    reg update

--- src/src/html/index.php
+++ src/src/html/index.php
@@ -3,8 +3,17 @@
 <body>
 <div class="container">
     <h1>Rec Dawgs Homepage</h1>
+    
+   
     <p>
-
+        <form method="POST" action=""> 
+             
+            
+            
+            
+            <input type="submit" value="showLeagues"> 
+        </form>
+        
     </p>
 </div
 </body>
