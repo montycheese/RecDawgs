b748408a5ee6787566e2838794fb95c7c9dd4019
Montana <montanawong@gmail.com>
2016-04-24 16:21:53 -0400

    small logic error

--- src/src/html/php/auth.php
+++ src/src/html/php/auth.php
@@ -10,10 +10,15 @@ use edu\uga\cs\recdawgs\presentation\AuthUI as AuthUI;
 
 try {
     $authUI = new AuthUI();
-    $message = urlencode(
-        $authUI->validateLogin($_POST['userName'], $_POST['password'])
-    );
-    header("Location: ../index.php?=status={$message}");
+    $message = $authUI->validateLogin($_POST['userName'], $_POST['password']);
+    if($message !=  "Login created, session started"){
+        $message = urlencode($message);
+        header("Location: ../login.php?=status={$message}");
+    }
+    else {
+        $message = urlencode($message);
+        header("Location: ../index.php?=status={$message}");
+    }
 }
 catch(Exception $e){
     $message = urlencode("Unexpected Error during authentication");
