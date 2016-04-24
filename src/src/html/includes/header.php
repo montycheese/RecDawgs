481a3f78d0840420d59ae6a1eccd876e6a226766
Montana <montanawong@gmail.com>
2016-04-24 18:31:39 -0400

    removed debug

--- src/src/html/includes/header.php
+++ src/src/html/includes/header.php
@@ -4,9 +4,9 @@ session_start();
 $url = 'http://' . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
 
 if (strpos($url,'login.php') === false) {
-    die(var_dump($_SESSION));
+    //die(var_dump($_SESSION));
     if (!isset($_SESSION) || !isset($_SESSION['userObject'])){
-        die(var_dump($_SESSION));
+      //  die(var_dump($_SESSION));
         //redirect to login page
         header("Location: login.php");
         exit();
