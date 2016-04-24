23d67fc2548f93562ad2f7f7a38b855b99ab12c6
unknown <qianghaoqiang@gmail.com>
2016-04-24 17:43:47 -0400

    league presentation and league html

--- src/src/html/leagues.php
+++ src/src/html/leagues.php
@@ -11,59 +11,15 @@ include('includes/header.php');
     <h3>Indoor:</h3>
 
     <?php
-    // to do: check the indoor type
-    $leagueModel = $_SESSION['logicLayer']->createLeague(
-    null, null, null, True, null, null, null, null);
-
-    $leaguesIndoor = $_SESSION['logicLayer']->findLeague($leagueModel);
-
-    $leagueIndoor = $leaguesIndoor->current();
-
-    if ($leaguesIndoor->size() == 0) {
-        echo "<p>No indoor leagues</p>";
-    } else {
-        echo '<form method="POST" action="league.php">';
-        echo '<select class="form-control">';
-
-        while ($leagueIndoor != null) {
-            $leagueName = $leagueIndoor->getName();
-            echo '<option value = "'.$leagueName.'">'.$leagueName.'</option>';
-            $leagueIndoor = $leaguesIndoor->next();
-        }
-
-        echo '</select>';
-        echo '<p><input type="submit" value = "Select League"></p>';
-        echo '</form>';
-    }
+    $leagueUI = new Presentation\LeagueUI();
+    echo $leagueUI->listIndoor();
     ?>
 
     <h3>Outdoor:</h3>
 
     <?php
-    // to do: check the indoor type
-    $leagueModel = $_SESSION['logicLayer']->createLeague(
-    null, null, null, False, null, null, null, null);
-
-    $leaguesIndoor = $_SESSION['logicLayer']->findLeague($leagueModel);
-
-    $leagueIndoor = $leaguesIndoor->current();
-
-    if ($leaguesIndoor->size() == 0) {
-        echo "<p>No outdoor leagues</p>";
-    } else {
-        echo '<form method="POST" action="league.php">';
-        echo '<select class="form-control">';
-
-        while ($leagueIndoor != null) {
-            $leagueName = $leagueIndoor->getName();
-            echo '<option value = "'.$leagueName.'">'.$leagueName.'</option>';
-            $leagueIndoor = $leaguesIndoor->next();
-        }
-
-        echo '</select>';
-        echo '<p><input type="submit" value = "Select League"></p>';
-        echo '</form>';
-    }
+    $leagueUI = new Presentation\LeagueUI();
+    echo $leagueUI->listOutdoor();
     ?>
    
 </div>
