2b5c0206f2f21026f1fe3dd70f1282b736e78291
Montana <montanawong@gmail.com>
2016-04-24 15:50:42 -0400

    added code to populate dropdown

--- src/src/html/teams.php
+++ src/src/html/teams.php
@@ -2,11 +2,15 @@
 
 <body>
     <div class="container">
-        <h3>Teams</h3>
+        <h3>My Teams</h3>
         
         <select name="teams" id="teams">
-
+            <option value="-1">---SELECT TEAM TO VIEW---</option>
+            <?php
+            $teamUI = new Presentation\TeamUI();
+            echo $teamUI->listAllContainingStudent($_SESSION['userObject']);?>
         </select>
 
     </div>
 </body>
+</html>
