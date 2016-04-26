d799bd5651bd67eba0799554f8a194757b6531d7
Montana <montanawong@gmail.com>
2016-04-26 10:19:23 -0400

    adding match pages

---
+++ src/src/html/enterMatchScore.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: montanawong
+ * Date: 4/26/16
+ * Time: 10:15
+ */
+
+//todo -
+//show a simple form with 2 fields, one for home score and one for away score
+//later- also add logic to prevent non captains of the matches from inputting score.
+////form should send user to doEnterMatchScore.php
---
+++ src/src/html/match.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: montanawong
+ * Date: 4/26/16
+ * Time: 10:16
+ */
+include("includes/header.php");
+//todo show the round# of the match, the league, the teams, and the scores(if the game has played), the date, etc.
+
+?>
