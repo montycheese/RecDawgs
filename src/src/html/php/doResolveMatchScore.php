39b2649247c1b9d5431b0554ba8efceec284e051
skhan731 <skhan731@uga.edu>
2016-04-29 15:18:57 -0400

    resolvematchscore

---
+++ src/src/html/php/doResolveMatchScore.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: skhan731
+ * Date: 4/29/2016
+ * Time: 3:15 PM
+ */
+
+require_once("autoload.php");
+use edu\uga\cs\recdawgs\logic\impl\LogicLayerImpl as LogicLayerImpl;
+
+$logicLayer = new LogicLayerImpl();
+$matchId = $_POST['matchId'];
+
+try {
+    // find match
+    $match = $logicLayer->findMatch(null, $matchId);
+    
+    
+} catch(\edu\uga\cs\recdawgs\RDException $rde){
+    $error_msg = urlencode($rde->string);
+    header("Location: ../resolveMatchScore.php?status={$error_msg}");
+}
+catch(Exception $e){
+    $errorMsg = urlencode("Unexpected error");
+    header("Location: ../resolveMatchScore.php?status={$errorMsg}");
+}
+exit();
